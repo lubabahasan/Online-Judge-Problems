6a87d682d63d636f7f86ba37bdc98edacacfec95
Lubaba Hasan <112156742+lubabahasan@users.noreply.github.com>
2023-12-20 09:45:31 +0600

    updates

--- Beecrowd/1000-1100/1042_simplesort.c
+++ Beecrowd/1000-1100/1042_simplesort.c
@@ -2,26 +2,28 @@
 int main()
 {
     int a,b,c,x,y;
-    scanf("%d %d %d",&a,&b,&c);
+    scanf("%d%d%d",&a,&b,&c);
+
     if (a>b)
     {
-         x=a,y=b;
+        x=a,y=b;
     } else
     {
         x=b,y=a;
     }
 
-    if (x>c&&y>c)
+    if (x>c&&c>y)
+    {
+        printf("%d\n%d\n%d\n",y,c,x);
+    } else if (x>c&&y>c)
     {
         printf("%d\n%d\n%d\n",c,y,x);
-    } else if (x>c&&c>y)
-    {
-      printf("%d\n%d\n%d\n",y,c,x);
     } else
     {
         printf("%d\n%d\n%d\n",y,x,c);
     }
 
     printf("\n%d\n%d\n%d\n",a,b,c);
+    
     return 0;
 }
